be3282d670b607923ea4c39ad5543eac700e85ca
i3arsu <lukkalukaa@gmail.com>
2025-10-23 16:51:16 +0200

    Initial Commit

---
+++ Predavanje1/src/components/Counter.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>Counter: {{ counter }}</h1>
+        <button @click="doubleCount">Double Count</button>
+        <button @click="decrement">Decrement</button>
+        <button @click="increment">Increment</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+    initialCount: {
+        type: Number,
+        default: 0
+    }
+})
+
+const counter = ref(0)
+counter.value = props.initialCount
+
+function increment() {
+    counter.value++
+}
+function decrement() {
+    counter.value--
+}
+
+function doubleCount() {
+    counter.value = counter.value * 2
+}
+
+</script>
+
+<style scoped>
+button {
+    margin: 0 5px;
+    padding: 5px 10px;
+    font-size: 16px;
+}
+</style>
---
+++ Predavanje1/src/views/CounterView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="counter-view">
+        <Counter :initial-count="5"/>
+    </div>
+</template>
+
+
+<script setup>
+import Counter from '../components/Counter.vue'
+</script> 
